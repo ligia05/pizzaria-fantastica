89e708ef89db5c975d746e0bf8facfb9289c3198
ligia05 <92107626+ligia05@users.noreply.github.com>
2021-12-14 09:12:16 -0300

    14_12

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,49 +1,90 @@
-const { application } = require('express');
-const pizzas = require('../database/Pizzas.json')
-const fs= require ('fs')
+const pizzas = require('../database/Pizzas.json');
+const fs = require('fs');
+const { validationResult } = require('express-validator');
+
+
 const controller = {
-    listar: (req, res) => {
-        return res.render('index', { pizzas, busca: "" });
-
-        //res.send(pizzas)
+    
+    listar: (req, res)=> {
+        return res.render('index',{pizzas, busca:""});
+        // res.send(pizzas)
     },
+
     getPizza: (req, res) => {
-        //res.send("tentando carregar UMA pizza!")
-        const idPizza = req.params.id;
-        const pizza = pizzas.find(p => p.id == idPizza);
-        res.render("pizza", { pizza });
-    },
-    busca: (req, res) => {
-        const stringPizzas = req.query.q.trim();
 
-        const pizzasFiltradas = pizzas.filter(
-            p => p.nome.toUpperCase().includes(stringPizzas.toUpperCase())
+        // Capturar o id requisitado (req.params)
+        const idPizza = req.params.id;
+        let idPrev = null;
+        let idNext = null;
+
+        // Capturar do array a pizza com o id requisitado (pizzas.find)
+        const pizza = pizzas.find(
+            (p, i) => {
+                idPrev = pizzas[i-1]==undefined?undefined:pizzas[i-1].id;
+                idNext = pizzas[i+1]==undefined?undefined:pizzas[i+1].id;
+                return p.id == idPizza
+            });
+
+        // Retornar a pizza encontrada para o cliente (res.send())
+        res.render('pizza',{pizza, idNext, idPrev});
+
+    },
+
+    busca: (req,res) => {
+
+        // Capturar a string digitada pelo visitante
+        const string = req.query.q.trim();
+
+        // Filtrar do arrays de pizzas somente as pizzas
+        // que que tiverem a string buscada no nome
+        const pizzasFiltras = pizzas.filter(
+            p => p.nome.toUpperCase().includes(string.toUpperCase())
         );
 
-
-
-        res.render('index', { pizzas: pizzasFiltradas, busca: stringPizzas });
+        // Renderizar a view index passando para ela
+        // as pizzas filtradas
+        res.render('index', {pizzas:pizzasFiltras, busca:string});
     },
-    create: (req,res) =>{
 
-  res.render("crud-pizzas/create")
+    create: (req, res) => {
+        res.render('crud-pizzas/create')
     },
-store: (req,res)=>{
-    const nome= req.body.nome;
-    const ingredientes = req.body.ingredientes.split(",").map(a => a.trim());
-    const preco = req.body.preco;
-    const pizza = {nome, ingredientes,preco, img:'/img/'+ req.file.filename}
-  pizza.id = pizzas[pizzas.length-1].id + 1;
-  
-  pizzas.push(pizza);
-  
-  fs.writeFileSync(__dirname +'/../database/Pizzas.json',
-   JSON.stringify(pizzas,null,4)),{flag:'w'}
 
-  
-  
-    res.redirect('/');
+    store: (req,res) => {
+
+        const erros = validationResult(req);
+        
+        if(!erros.isEmpty()){
+            // return res.send(erros.mapped());
+            res.render('crud-pizzas/create', {erros: erros.mapped()})
+        }
+
+        const nome = req.body.nome;
+        const ingredientes = req.body.ingredientes.split(',').map(a => a.trim());
+        const preco = Number(req.body.preco);
+        const pizza = {nome, ingredientes, preco, img:'/img/' + req.file.filename}
+        
+        // Adicionar o id à pizza recém criada
+        pizza.id = pizzas[pizzas.length - 1].id + 1;
+
+        // Adicionar a pizza ao array de pizzas
+        pizzas.push(pizza);
+
+        // Salvar o json do array de pizzas no arquivo Pizzas.json
+        fs.writeFileSync(
+            __dirname + '/../database/Pizzas.json',
+            JSON.stringify(pizzas, null, 4),
+            {flag:'w'}
+        );
+        
+        // Direcionar o usuário para a página que exibe a lista de pizzas
+        res.redirect('/');
+
+    }
+
+
 
 }
-}
+
 module.exports = controller;
+
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const PizzasRouter= require('./routers/PizzasRouter')
-const AdmRouter = require ("./routers/AdmRouters") 
+const AdmRouter = require ("./routers/AdmRouters"); 
+const LogMiddleware = require('./middlewares/LogMiddleware');
+//const LogMiddleware=
 const app = express()
+
 app.set("view engine", "ejs");
 app.set("views","./views");
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+//app.use(LogMiddleware)
 app.use(express.static(__dirname + "/public"));
 app.use('/', PizzasRouter);
 app.use("/", AdmRouter);
